a8c9fef4673873d6cec7769cd9df14d0a30a6e9a
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2021-10-07 23:49:54 +0100

    added glow effect intreaction to project

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const heroCamera = document.querySelectorAll('.hero-camera');
 const heroImages = document.querySelectorAll('.hero-image');
-// const heroImagesBottom = document.querySelectorAll('.hero-image .bottom');
+const heroImagesBottom = document.querySelectorAll('.hero-image .bottom');
+const selectedEffect = document.querySelectorAll('.selected-project-effect');
 
 const nav = document.querySelector('.nav');
 const hamburgerIcon = document.querySelector('.hamburger');
@@ -8,3 +9,15 @@ const hamburgerIcon = document.querySelector('.hamburger');
 hamburgerIcon.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
+
+// console.log(selectedEffect)
+
+selectedEffect.forEach(effect => {
+    const project = effect.parentElement;
+    project.addEventListener('mouseenter', () => {
+        effect.classList.add('glow');
+    });
+    project.addEventListener('mouseleave', () => {
+        effect.classList.remove('glow');
+    });
+})
