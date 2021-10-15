169a835b88a3bd4885c3b44d7cfc9c5e3085ad8e
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2021-10-15 13:58:33 +0100

    fixing footer bug

--- script.js
+++ script.js
@@ -2,14 +2,6 @@ const heroCamera = document.querySelectorAll('.hero-camera');
 const heroImages = document.querySelectorAll('.hero-image');
 // const heroImagesBottom = document.querySelectorAll('.hero-image .bottom');
 
-addEventListener('scroll', () => {
-    heroImages.forEach(image => {
-        image.style.animation = 'moveleft ease';
-        // const top =  heroCamera.boundingClientRect().top;
-        console.log(top);
-    });
-});
-
 const nav = document.querySelector('.nav');
 const hamburgerIcon = document.querySelector('.hamburger');
 
