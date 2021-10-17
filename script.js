4d0dff2cf3d932cd7e7993c986a20b40e4dc8a31
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2021-10-17 20:58:37 +0100

    added success lottie files to the form and redesigned from light to dark

--- script.js
+++ script.js
@@ -2,50 +2,28 @@ const heroCamera = document.querySelectorAll('.hero-camera');
 const heroImages = document.querySelectorAll('.hero-image');
 const heroImagesBottom = document.querySelectorAll('.hero-image .bottom');
 const selectedEffect = document.querySelectorAll('.selected-project-effect');
-
 const nav = document.querySelector('.nav');
 const hamburgerIcon = document.querySelector('.hamburger');
+const form = document.querySelector('form');
+const success = document.querySelector('.success');
+const successEffect = document.querySelector('.success-json');
 
 hamburgerIcon.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
 
-window.addEventListener('loadstart', () => {
-    
-})
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    success.classList.add('active');
+    successEffect.play();
+    form.reset();
+    setTimeout(() => {
+        success.classList.remove('active');
+    }, 4000)
+});
 
-// scroll animation
 
-// const lastScrollTop = 0;
-// addEventListener("scroll", () => { 
-//    const st = window.pageYOffset;
-//    if (st > lastScrollTop){
-//       // downscroll code
-//       heroImages.forEach(image => {
-//           image.style.transform = 'translateX(-500px)';
-//       })
-//       console.log('down');
-//    } else {
-//       // upscroll code
-//       console.log('up');
-//    }
-// //    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
-// }, false);
-
-// var lastScroll = 0;
-
-//   window.onscroll = function() {
-//       let currentScroll =  document.body.scrollTop; // Get Current Scroll Value
-
-//       if (currentScroll > 0 && lastScroll <= currentScroll){
-//         lastScroll = currentScroll;
-//         console.log('down');
-//       }else{
-//         lastScroll = currentScroll;
-//         console.log('up');
-//     }
-//   };
-
+// glow effect interaction
 const desktop = window.matchMedia("(min-width: 1000px)")
 
 if(desktop.matches) {
