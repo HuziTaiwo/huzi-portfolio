e9bb07d2c8365976573904ec57135ba370bb0b3f
taiwohusayn5@gmail.com <taiwohusayn5@gmail.com>
2021-10-18 18:45:45 +0100

    added 404 interaction

--- 404.js
+++ 404.js
@@ -1,4 +1,18 @@
+const interaction = document.querySelector('.interaction-404');
 // 404 interaction
-window.addEventListener('mousemove', () => {
-    console.log('moved')
+window.addEventListener('mousemove', (e) => {
+    let top = e.pageY;
+    let left = e.pageX;
+    interaction.style.top = `${e.pageY}px`;
+    interaction.style.left = `${e.pageX}px`;
 });
+
+/*
+transform: translate3d(29.9706vw, -10.329vh, 0px) scale3d(1, 1, 1) rotateX(
+0deg) rotateY(
+0deg) rotateZ(
+0deg) skew(
+0deg, 
+0deg);
+    transform-style: preserve-3d;
+*/
